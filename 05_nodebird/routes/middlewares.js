76a76805c5bbdafec6116e04c642e86cecc8e850
robbin <gooodev402@gmail.com>
2018-11-25 00:30:09 +0900

    add local passport strategy

---
+++ 05_nodebird/routes/middlewares.js
@@ -0,0 +1,16 @@
+// 로그인 중 일때, req.isAuthenticated : true
+exports.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(403).send("로그인 필요");
+  }
+};
+
+exports.isNotLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
